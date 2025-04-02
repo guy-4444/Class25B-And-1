942f6d2723042aaf0021d4b7e4d2ac1316f83df0
guy <guyskv@gmail.com>
2025-04-02 18:32:32 +0300

    Linear Layout View Binding Glide

---
+++ app/src/main/java/com/guyi/class25b_and_1/GameActivity.java
@@ -0,0 +1,39 @@
+package com.guyi.class25b_and_1;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.guyi.class25b_and_1.databinding.ActivityGameBinding;
+
+public class GameActivity extends AppCompatActivity {
+
+    private ActivityGameBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityGameBinding.inflate(getLayoutInflater());
+
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Glide
+                .with(this)
+                .load(R.drawable.img_back)
+                .into(binding.imgBackground);
+
+    }
+}
